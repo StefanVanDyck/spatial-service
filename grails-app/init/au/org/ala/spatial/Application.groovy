7fcbf819b38e667c711c2c4aed3dad90f64ee4a4
Stefan Van Dyck <vandyck.stefan@gmail.com>
2024-07-05 13:50:04 +0200

    try to fix db trasnaction issue

--- grails-app/init/au/org/ala/spatial/Application.groovy
+++ grails-app/init/au/org/ala/spatial/Application.groovy
@@ -6,9 +6,11 @@ import groovy.transform.CompileStatic
 import org.grails.io.support.PathMatchingResourcePatternResolver
 import org.grails.io.support.Resource
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @CompileStatic
 @ComponentScan
+@EnableTransactionManagement
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
 
